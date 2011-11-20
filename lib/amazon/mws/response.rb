47b136aacd211e91549feaec86c1a11b247cf3e8
Radu Voicilas <radu.voicilas@gmail.com>
2011-11-20 14:15:26 +0200

    Moved ResponseError in response.rb
    It's easier to get rid of the whole response directory without impacting
    too much the way responses are built.

--- lib/amazon/mws/response.rb
+++ lib/amazon/mws/response.rb
@@ -1,19 +1,19 @@
 module Amazon
   module MWS
-    
+
     class Response
       include ROXML
       xml_convention :camelcase
-      
+
       # This is the factoryish method that is called!, not new
       def self.format(response)
         if response.content_type =~ /xml/ || response.body =~ /<?xml/
-          parse_xml(response)  
+          parse_xml(response)
         else
           response.body
         end
       end
-      
+
       def self.parse_xml(response)
         if [Net::HTTPClientError, Net::HTTPServerError].any? {|error| response.is_a? error }
           return ResponseError.from_xml(response.body)
@@ -21,11 +21,21 @@ module Amazon
           return self.from_xml(response.body)
         end
       end
-      
+
       def accessors
         roxml_references.map {|r| r.accessor}
       end
-      
+
+    end
+
+    class ResponseError < Response
+      xml_name "ErrorResponse"
+
+      xml_reader :type, :in => "Error"
+      xml_reader :code, :in => "Error"
+      xml_reader :message, :in => "Error"
+      xml_reader :detail, :in => "Error"
+      xml_reader :request_id
     end
 
   end
